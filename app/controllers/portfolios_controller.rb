981ebccf8b0f17d55c31af0525eb57bd63da4ca1
colkar99 <colkar99@gmail.com>
2018-08-21 17:54:06 +0530

    asas

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,10 +1,12 @@
 class PortfoliosController < ApplicationController
 	def index
 		@portfolio_items = Portfolio.all 		
-	end	
+	end
+
 	def new
 		@portfolio_item = Portfolio.new
 	end
+
 	def create
 	   @portfolio_item = Portfolio.new(portfolio_params)
 
@@ -17,9 +19,29 @@ class PortfoliosController < ApplicationController
 	        format.json { render json: @portfolio_item.errors, status: :unprocessable_entity }
 	      end
 	    end
-	end    
+	end
+
+	def edit
+	    @portfolio_item = Portfolio.find(params[:id]) 	
+	end
+
+	def update
+		 @portfolio_item = Portfolio.find(params[:id]) 
+	    respond_to do |format|
+	      if @portfolio_item.update(portfolio_params)
+	        format.html { redirect_to portfolios_path, notice: 'portfolio was successfully updated.' }
+	        # format.json { render :show, status: :ok, location: @blog }
+	      else
+	        format.html { render :edit }
+	        # format.json { render json: @blog.errors, status: :unprocessable_entity }
+	      end
+	    end
+	end   
+
     private
+
     def portfolio_params
     	params.require(:portfolio).permit(:title,:subtitle, :body)
     end
+
   end
