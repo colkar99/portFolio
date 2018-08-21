0678ed934d1963901761a33af0bb18eeaef7d093
colkar99 <colkar99@gmail.com>
2018-08-21 12:10:19 +0530

    model generator completed

---
+++ db/migrate/20180821063133_create_skills.rb
@@ -0,0 +1,11 @@
+class CreateSkills < ActiveRecord::Migration[5.2]
+  def change
+    create_table :skills do |t|
+      t.string :title
+      t.integer :percent_utilized
+      t.text :body
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_21_045109) do
+ActiveRecord::Schema.define(version: 2018_08_21_063133) do
 
   create_table "blogs", force: :cascade do |t|
     t.string "title"
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "skills", force: :cascade do |t|
+    t.string "title"
+    t.integer "percent_utilized"
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
