6d8897535a78aa7613b44e89c1ef461c8d583ba8
colkar99 <colkar99@gmail.com>
2018-08-22 09:53:31 +0530

    implemented destroy action

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -39,6 +39,14 @@ class PortfoliosController < ApplicationController
 	end
 	def show
 	    @portfolio_item = Portfolio.find(params[:id])	
+	end
+	def destroy
+	    @portfolio_item = Portfolio.find(params[:id])
+	    @portfolio_item.destroy
+    	respond_to do |format|
+      		format.html { redirect_to portfolios_path, notice: 'portfolio was successfully destroyed.' }
+      # format.json { head :no_content }
+    	end	
 	end   
 
     private
