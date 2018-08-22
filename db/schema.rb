da97bf6933da7682dad951639ab2dd33b1459e10
colkar99 <colkar99@gmail.com>
2018-08-22 17:45:21 +0530

    implemented concern for data management for imgae

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_22_105133) do
+ActiveRecord::Schema.define(version: 2018_08_22_115720) do
 
   create_table "blogs", force: :cascade do |t|
     t.string "title"
@@ -52,6 +52,7 @@ ActiveRecord::Schema.define(version: 2018_08_22_105133) do
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "badge"
   end
 
   create_table "topics", force: :cascade do |t|
