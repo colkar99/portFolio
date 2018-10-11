de5b082b970dda6b316200d7799554ce86308f88
colkar99 <colkar99@gmail.com>
2018-10-11 17:06:26 +0530

    updated file upload

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -2,8 +2,8 @@ class Portfolio < ApplicationRecord
 	has_many :technologies
 	accepts_nested_attributes_for :technologies,
 									reject_if: lambda { |attrs| attrs['name'].blank? }
-	include Placeholder
-	validates_presence_of :title,:subtitle,:main_image,:body,:thumb_image
+	#include Placeholder
+	validates_presence_of :title,:subtitle,:body
 
 	#this lines for image up
 	mount_uploader :thumb_image, PortfolioUploader
@@ -20,14 +20,10 @@ class Portfolio < ApplicationRecord
 
 	scope :ruby_on_rails_portfolio_items, -> { where(subtitle: 'Ruby on Rails')}
 
-	after_initialize :set_defaults
-
-	def set_defaults
-		self.main_image ||= Placeholder.image_generator(height: '600',width: '400')
-		self.thumb_image ||= Placeholder.image_generator(height: '350',width: '200')
-	end
+	#after_initialize :set_defaults
 	def self.select_all
 		all
 	end
 	
 end
+
