d090cd74602181ae539687cdcbc312cfede68ac0
colkar99 <colkar99@gmail.com>
2018-10-26 15:23:28 +0530

    implemented custom scope for recent blogs

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -5,6 +5,10 @@ class Blog < ApplicationRecord
   	validates_presence_of :title,:body
   	belongs_to :topic 
   	has_many :comments , dependent: :destroy
+
+  	def self.recent
+  		order("created_at DESC")
+  	end
 end
 
 
